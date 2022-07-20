4a0808b6554f6963be328ba392b08a3ae2183a52
Angel Travieso <angeltraviesoc@gmail.com>
2022-07-20 08:51:44 -0400

    inferencia de tipos con var y primitivos

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -160,6 +160,19 @@ public class HolaMundo {
          */
         
         
+        
+        var numeroEntero = 10;
+        System.out.println("numeroEntero = " + numeroEntero);
+        
+           
+        var numeroDouble = 10.0;
+        System.out.println("numeroDouble = " + numeroDouble);
+        
+        var numeroFloat = 10.0F;
+        System.out.println("numeroFloat = " + numeroFloat);
+        
+        
+        
     }
 
 }

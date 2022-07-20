20cb97c37f51cb2e16614d0bccc8705ea48e8511
Angel Travieso <angeltraviesoc@gmail.com>
2022-07-20 17:13:39 -0400

    tiendas de libros

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -208,11 +208,10 @@ public class HolaMundo {
         int letra = 'a';
         System.out.println("letra = " + letra);
          */
- 
-         /*
+ /*
  tipos primitivos booleanos
- */
-         
+         */
+ /*
          boolean varBoolean = false;
          System.out.println("varBoolean = " + varBoolean);
          
@@ -229,9 +228,54 @@ public class HolaMundo {
              System.out.println("Eres mayor de edad");
          } else {
              System.out.println("Eres menor de edad");
-         }
-         
-         
+         }*/
+        // Conversion de tipos primitivos en Java
+        // Convertir tipo String a un tipo int
+        /*
+        var edad = Integer.parseInt("20");
+        // var edad = "20";
+        System.out.println("edad = " + (edad + 1));
+
+        var valorPi = Double.parseDouble("3.1416");
+        System.out.println("valorPi = " + valorPi);
+
+        // Pedir un valor
+        var consola = new Scanner(System.in);
+//        System.out.println("Proporciona tu edad:");
+//        edad = Integer.parseInt( consola.nextLine() );
+//        System.out.println("edad = " + edad);
+
+        var edadTexto = String.valueOf(10);
+        System.out.println("edadTexto = " + edadTexto);
+
+        // Recuperar caracter de una cadena
+        var caracter = "hola".charAt(1);
+        System.out.println("caracter = " + caracter);
+
+        System.out.println("Proporciona un caracter:");
+        caracter = consola.nextLine().charAt(0);
+
+        System.out.println("caracter = " + caracter);
+         */
+        
+        Scanner consola = new Scanner(System.in);
+        
+        System.out.println("Proporciona el nombre:");
+        var nombre = consola.nextLine();
+        
+        System.out.println("Proporciona el id:");
+        var id = Integer.parseInt(consola.nextLine());
+        
+        System.out.println("Proporciona el precio:");
+        var precio = Double.parseDouble(consola.nextLine());
+        
+        System.out.println("Proporciona el envio gratuito:");
+        var envio = Boolean.parseBoolean(consola.nextLine());
+        
+        System.out.println(nombre + " #" + id);
+        System.out.println("Precio: $" + precio);
+        System.out.println("Envio Gratuito: " + envio);
+        
     }
 
 }

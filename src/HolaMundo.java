3edcc06ac6e7e81b33272e2d60a8e5aa97da74e7
Angel Travieso <angeltraviesoc@gmail.com>
2022-07-22 08:27:17 -0400

    operadores de asignacion

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -277,7 +277,7 @@ public class HolaMundo {
         System.out.println("Envio Gratuito: " + envio);
          */
         // Operadores aritméticos en Java
-        
+        /*
         int a = 3, b = 2;
         var resultado = a + b;
         
@@ -300,7 +300,35 @@ public class HolaMundo {
             System.out.println("Es numero par");
         else
             System.out.println("Es numero impar");
+*/
         
+        // Operadores de asignacion
+        
+        int a = 3, b = 2;
+        int c = a + 5 - b;
+        
+        System.out.println("c = " + c);
+        
+        a += 1; // a = a +1
+        
+        System.out.println("a = " + a);
+        
+        
+        a += 3; // a = a + 3
+        System.out.println("a = " + a);
+        
+        a -= 2; // a = a - 2
+        System.out.println("a = " + a);
+        
+        // *= , /= , %=
+        a *= 2;
+        System.out.println("a = " + a);
+        
+        a /= 2;
+        System.out.println("a = " + a);
+        
+        a %= 2;
+        System.out.println("a = " + a);
         
         
         

2c5d6b4a4dab1292d26960372cb0b8dc2dbee393
Angel Travieso <angeltraviesoc@gmail.com>
2022-08-04 17:08:16 -0400

    ejercicio: calculo estacion del año con switch

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -554,7 +554,7 @@ public class HolaMundo {
         System.out.println("Estas en " + estacion + "...");
          */
         // Switch
-        
+        /*
         var numero = 2;
         var numeroTexto = "Desconocido";
         
@@ -578,6 +578,29 @@ public class HolaMundo {
         }
         
         System.out.println("numeroTexto = " + numeroTexto);
+         */
+        // Ejercicio: Calculo estacion del año con switch
+        
+        var mes = 1;
+        
+        var estacion = "Estacion desconocida";
+        
+        switch( mes ) {
+            case 1: case 2: case 12:
+                estacion = "Invierno";
+                break;
+            case 3: case 4: case 5:
+                estacion = "Primavera";
+                break;
+            case 6: case 7: case 8:
+                estacion = "Verano";
+                break;
+            case 9: case 10: case 11:
+                estacion = "Otonio";
+                break;
+        }
+        
+        System.out.println("estacion = " + estacion);
         
     }
 

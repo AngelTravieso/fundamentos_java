3799b2e9dfba0c0b64e176d7e34f90911f84534d
Angel Travieso <angeltraviesoc@gmail.com>
2022-08-08 08:42:26 -0400

    clase persona (poo)

---
+++ src/clases/Persona.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author LibrePago-02
+ */
+public class Persona {
+    
+    // Atributos de la clase
+    String nombre;
+    String apellido;
+    
+    // Metodos de la clase
+    public void desplegarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+    }
+    
+}

d4d8eb14e2407c03139ebdd323f5d2d5008a93f6
Angel Travieso <angeltraviesoc@gmail.com>
2022-08-09 16:58:21 -0400

    proyecto caja

---
+++ Caja/src/PruebaCaja.java
@@ -0,0 +1,22 @@
+
+import operaciones.Caja;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LibrePago-02
+ */
+public class PruebaCaja {
+    
+    public static void main(String[] args) {
+        
+        Caja caja1 = new Caja(2, 3, 6);
+        caja1.calcularVolumen();
+        
+    }
+    
+}

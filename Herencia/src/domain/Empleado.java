a7d7fabd638d35361ff8178cfdec449e3ff14a7b
Angel Travieso <angeltraviesoc@gmail.com>
2022-10-24 17:06:49 -0400

    herencia en java

---
+++ Herencia/src/domain/Empleado.java
@@ -0,0 +1,42 @@
+package domain;
+
+public class Empleado extends Persona {
+    
+    private int idEmpleado;
+    private double sueldo;
+    private static int contadorEmpleado;
+
+    public Empleado(String nombre, double sueldo) {
+        // Llamar constructor de la clase padre
+        super(nombre);
+        this.idEmpleado = ++Empleado.contadorEmpleado;
+        this.sueldo = sueldo;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Empleado{");
+        sb.append("idEmpleado=").append(this.idEmpleado);
+        sb.append(", sueldo=").append(this.sueldo);
+        sb.append(", ").append(super.toString());
+        return sb.toString();
+    }
+  
+}

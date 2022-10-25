d133bfac2cb51e984ca686233879e5f9f16300b4
AngelTravieso <angeltraviesoc@gmail.com>
2022-10-24 21:19:34 -0400

    herencia (terminado)

--- Herencia/src/test/TestHerencia.java
+++ Herencia/src/test/TestHerencia.java
@@ -1,10 +1,21 @@
 package test;
 
+import domain.Cliente;
 import domain.Empleado;
+import java.util.Date;
 
 public class TestHerencia {
     public static void main(String[] args) {
-        Empleado empleado1 = new Empleado("Juan", 5000.0);
-        System.out.println("empleado1 = " + empleado1);
+//        Empleado empleado1 = new Empleado("Juan", 5000.0);
+//        System.out.println("empleado1 = " + empleado1);
+
+        // new Date: genera un objeto Date con la fecha actual
+        var fecha = new Date();
+            
+        Cliente cliente1 = new Cliente(
+                fecha, true, "Karla", 'F', 28, "Saturno");
+        
+        System.out.println("cliente1 = " + cliente1);
+
     }
 }

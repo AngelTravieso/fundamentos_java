1db29988dc37f82c020904bcef3219fcff9f5882
Angel Travieso <angeltraviesoc@gmail.com>
2022-11-04 17:15:31 -0400

    arreglos

---
+++ Arreglos/src/test/Test.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Test {
+   public static void main(String[] args) {
+       // declarar arreglo de int
+       // []edades
+       
+       // debemos indicar la cantidad de elementos que va a almacenar (3)
+       // los arreglos no pueden crecer dinamicamente
+       
+       int edades[] = new int[3]; // los arreglos heredan de la clase object, por eso se usa new
+       
+       System.out.println("edades: " + edades); // edades: [I@682a0b20
+       
+   } 
+}

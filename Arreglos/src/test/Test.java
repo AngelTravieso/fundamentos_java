50e48321cc3029c5154e2c0744cee6688cdb8da9
Angel Travieso <angeltraviesoc@gmail.com>
2022-11-04 17:19:25 -0400

    imprimir valores de arreglo

--- Arreglos/src/test/Test.java
+++ Arreglos/src/test/Test.java
@@ -8,14 +8,20 @@ public class Test {
        // debemos indicar la cantidad de elementos que va a almacenar (3)
        // los arreglos no pueden crecer dinamicamente
        
+       // longitud de un arreglo: n - 2
+       
        int edades[] = new int[3]; // los arreglos heredan de la clase object, por eso se usa new
        
        System.out.println("edades: " + edades); // edades: [I@682a0b20
        
        // acceder a los elementos del arreglo [indice]
        edades[0] = 10;
+       edades[1] = 15;
+       edades[2] = 20;
        
        System.out.println("edades 0 = " + edades[0]);
+       System.out.println("edades 2 = " + edades[1]);
+       System.out.println("edades 3 = " + edades[2]);
        
    } 
 }

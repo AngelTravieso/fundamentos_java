f90f1bf3853aba85085064e5afeb71aa05c3661a
Angel Travieso <angeltraviesoc@gmail.com>
2022-11-04 17:18:14 -0400

    modificacion del valor de un arreglo

--- Arreglos/src/test/Test.java
+++ Arreglos/src/test/Test.java
@@ -12,5 +12,10 @@ public class Test {
        
        System.out.println("edades: " + edades); // edades: [I@682a0b20
        
+       // acceder a los elementos del arreglo [indice]
+       edades[0] = 10;
+       
+       System.out.println("edades 0 = " + edades[0]);
+       
    } 
 }

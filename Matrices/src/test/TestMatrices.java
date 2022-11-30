2eb9cdf63df73d9ad1a98ac0c402d85ef436f364
Angel Travieso <angeltraviesoc@gmail.com>
2022-11-30 11:37:08 -0400

    funcion para imprimir valores de arreglos

--- Matrices/src/test/TestMatrices.java
+++ Matrices/src/test/TestMatrices.java
@@ -1,5 +1,7 @@
 package test;
 
+import domain.Persona;
+
 public class TestMatrices {
     public static void main(String[] args) {
         
@@ -24,11 +26,35 @@ public class TestMatrices {
         // recorrer matriz con ciclo for anidado
         for(int i = 0; i < edades.length; i++) {
             for(int j = 0; j < edades[i].length; j++) {
-                System.out.println("edades[" + i + "][" + j + "]" + " = " + edades[i][j] ); 
+                System.out.println("edades[" + i + "][" + j + "]" + " = " + edades[i][j] );
             }
         }
         
+        // declarar e inicializar matriz (sintaxis resumida)
+        String frutas[][] = {
+            {"Naranja", "Limon"},
+            {"Fresa", "Zarzamora", "Mora"}
+        };
         
+        imprimir(frutas);
+        
+        
+        // matriz de tipo object
+        Persona personas[][] = new Persona[2][3];
+        
+        personas[0][0] = new Persona("Angel");
+        personas[0][1] = new Persona("Gustavo");
+        imprimir(personas);
         
     }
+       
+    // pasar referencia al objeto que se se pasa (la matriz)
+    public static void imprimir(Object matriz[][]) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.println("matriz[" + i + "][" + j + "]" + " = " + matriz[i][j]);
+            }
+        }
+    }
+    
 }

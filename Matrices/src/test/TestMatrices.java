7d53e218e78fa1237bf07e30c7056445a4498b3b
Angel Travieso <angeltraviesoc@gmail.com>
2022-11-30 11:11:56 -0400

    recorrer matrices con for anidado

--- Matrices/src/test/TestMatrices.java
+++ Matrices/src/test/TestMatrices.java
@@ -6,7 +6,29 @@ public class TestMatrices {
         // declarar matriz (arreglo bidimensional) [fila][columna]
         int edades[][] = new int[3][2];
         
-        System.out.println("edades = " + edades);
+//        System.out.println("edades = " + edades);
+        
+        // modificar valor de matriz para la posicion dada
+        edades[0][0] = 5;
+        edades[0][1] = 7;
+        
+        edades[1][0] = 8;
+        edades[1][1] = 4;
+        
+        edades[2][0] = 12;
+        edades[2][1] = 21;
+        
+        // acceder a los elementos (for)
+//        System.out.println("edades [0][0] = " + edades[0][0]);
+        
+        // recorrer matriz con ciclo for anidado
+        for(int i = 0; i < edades.length; i++) {
+            for(int j = 0; j < edades[i].length; j++) {
+                System.out.println("edades[" + i + "][" + j + "]" + " = " + edades[i][j] ); 
+            }
+        }
+        
+        
         
     }
 }
